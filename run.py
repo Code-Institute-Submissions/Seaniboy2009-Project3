5a1ff0f0395a822d15d6140e144457ad66d6a07b
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-08-25 18:36:23 +0000

    add fucntion to add ships to board

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
 
 
 class Board:
     """
     Create instance of the board class
     """
-    def __init__(self, name, size, ships):
+    def __init__(self, name, size, num_ships, type):
         self.name = name
         self.size = size
-        self.board = [["[]" for x in range(size)] for y in range(size)]
-        self.ships = ships
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.type = type
+        self.ships = []
+        self.misses = []
 
     def create_board(self):
         for row in self.board:
             print(" ".join(row))
+
+    def add_ship(self, x, y):
+        self.ships.append((x, y))
+        self.board[x][y] = "!"
+        # if self.type == "player":
+        #     self.board[x][y] = "!"
+        print(self.ships)
+
+    # def take_shot(self, x, y):
+    #     if self.board[x][y]
 
 
 def validate_input(input):
@@ -35,6 +46,23 @@ def validate_input(input):
     return True
 
 
+def add_ships_to_board(board):
+    """
+    Add player and computer ships to there boards using random.
+    """
+    num1 = return_num(board.size)
+    num2 = return_num(board.size)
+    board.add_ship(num1, num2)
+
+
+def return_num(num):
+    """
+    Returns a random number within the num and 0
+    """
+
+    return randint(0, num - 1)
+
+
 def start_game():
     """
     Initialize the start function, ask for player name, board size.
@@ -43,22 +71,31 @@ def start_game():
     print('Welcome to battleships, please have fun and remember to shout:')
     print('"YOU SUNK MY BATTLESHIP"\n')
 
-    name = input("Please enter your name:\n")
+    # Testing
+    name = "Player"
+    size = 2
+    ships = 2
 
-    while True:
+    # name = input("Please enter your name:\n")
 
-        size = int(input("Please pick board size, from 4x4 min to 8x8 max:\n"))
-        if validate_input(size):
-            break
+    # while True:
 
-    while True:
+    #     size = int(input("Please pick board size, Min 4: Max 8:\n"))
+    #     if validate_input(size):
+    #         break
 
-        ships = int(input("Please select number of ships, 4 min 8 max:\n"))
-        if validate_input(ships):
-            break
+    # while True:
 
-    player_b = Board(name, size, ships)
-    computer_b = Board("Computer", size, ships)
+    #     ships = int(input("Please select number of ships, Min 4: Max 8:\n"))
+    #     if validate_input(ships):
+    #         break
+
+    player_b = Board(name, size, ships, "player")
+    computer_b = Board("Computer", size, ships, "computer")
+
+    for ship in range(ships):
+        add_ships_to_board(player_b)
+        add_ships_to_board(computer_b)
 
     player_b.create_board()
     print("_" * (size * size))
